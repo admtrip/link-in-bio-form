816035a243b4e2d9cd54d5698cc9b9eb80355f1e
admtrip <atripat0@chicagobooth.edu>
2025-02-19 13:35:38 +0000

    complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-
-  get("/", { :controller => "items", :action => "index" })
+  get("/", { controller: "items", action: "index" })
+  get("/backdoor", { controller: "items", action: "backdoor" })
+  post("/insert_item", { controller: "items", action: "create" })
 end
